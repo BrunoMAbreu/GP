f18d262ed10b4c646b3e068acf50962f5a9ccbd5
navi <vosqueentrais@gmail.com>
2019-04-03 20:48:03 +0100

    server.js

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
-'use strict';
 
-let express = require("express");
-let bodyParser = require("body-parser");
-let routes = require('./src/routes/api.js');
-
-let app = express();
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-
-app.use('/api', routes);
-
-let server = app.listen(8081, function () {
-    const host = server.address().address === "::" 
-        ? "localhost" 
-        : server.address().address;
-    const port = server.address().port;
-    
-    // apagar antes de build
-    console.log("Example app listening at http://%s:%s", host, port);
-});
+console.log("Aplicação")
---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let express = require("express");
+let bodyParser = require("body-parser");
+let routes = require('./src/routes/api.js');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+
+app.use('/api', routes);
+
+let server = app.listen(8081, function () {
+    const host = server.address().address === "::" 
+        ? "localhost" 
+        : server.address().address;
+    const port = server.address().port;
+    
+    console.log("Example app listening at http://%s:%s", host, port);
+});
