5295a9343062588b1e038cd1cbe5a12db5ba7e1d
navi <vosqueentrais@gmail.com>
2019-04-11 20:46:44 +0100

    update

--- app.js
+++ app.js
@@ -3,9 +3,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require('./src/routes/api.js');
-const MongoClient = require('mongodb').MongoClient;
-//const ObjectId = require('mongodb').ObjectId;
-const mongoDBConfig = require("./src/model/db/mongoConfig.js");
 let app = express();
 app.use(bodyParser.urlencoded({
     extended: true
@@ -21,22 +18,6 @@ app.get('/healthz', function (req, res) {
     res.send('ok');
 });
 
-
-let db;
-let dbClient;
-
-/*
-MongoClient.connect(mongoDBConfig.url, { useNewUrlParser: true, autoReconnect: true }).then(client => {
-    db = client.db(mongoDBConfig.dbName);
-    dbClient = client;
-
-    console.log(1);
-    //collection = db.collection('my-collection');
-}).catch(error => console.error(error)); */
-
-// const collection = req.app.locals.collection; ------> para ler
-app.locals.teste = 'teste1' // apagar
-app.locals.dbConnection = db;
 let server = app.listen(8080, function () {
     const host = server.address().address === "::"
         ? "localhost"
@@ -46,13 +27,7 @@ let server = app.listen(8080, function () {
     console.log("App listening at http://%s:%s", host, port);
 })
 
-// Listen for the signal interruption (ctrl-c); Close the MongoDB connection
-/*
-process.on('SIGINT', () => {
-    dbClient.close();
-    process.exit();
-});
-*/
+
 exports.app = app;
 
 
--- src/model/db/mongoConfig.js
+++ src/model/db/mongoConfig.js
@@ -1,4 +1,56 @@
-module.exports = {
-    "dbName": "quintaDoMiao",
-    "url": process.env.MONGO_URL || "mongodb://localhost:27017/"
+const MongoClient = require('mongodb').MongoClient;
+//const ObjectId = require('mongodb').ObjectId;
+//const mongoDBConfig = require("./src/model/db/mongoConfig.js");
+
+
+let mongoDBConfig = {
+    "name": "quintaDoMiao",
+    "url": process.env.MONGO_URL || "mongodb://localhost:27017/",
+    "connection": null,
+    "collections": []
 }
+
+
+let connectMongoDB = function () {
+
+    // "== null" is correct, not "=== null"
+    if (mongoDBConfig.connection == null) {
+        MongoClient.connect(mongoDBConfig.url, { useNewUrlParser: true, autoReconnect: true }).then(client => {
+            let db = client.db(mongoDBConfig.name);
+            mongoDBConfig.collections.push({
+                "name": "Users",
+                "collection": db.collection("Users")
+            });
+            mongoDBConfig.connection= client;
+
+        
+
+
+            //console.log(mongoDBConfig.connection);
+
+            //console.log(mongoDBConfig.collections[0].name);
+            //collection = db.collection('my-collection');
+
+            //closeDBConnection();
+        }).catch(error => console.error(error));
+
+
+    }
+
+
+
+}
+
+connectMongoDB();
+
+
+
+
+// Listen for the signal interruption (ctrl-c); Close the MongoDB connection
+process.on('SIGINT', () => {
+    mongoDBConfig.connection.close();
+    process.exit();
+});
+
+module.exports.connectMongoDB = connectMongoDB;
+module.exports.mongoDBConfig = mongoDBConfig;
