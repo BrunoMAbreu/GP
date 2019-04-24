23c8937192642e94a34b37cca6e4f39d0d2b36a2
navi <vosqueentrais@gmail.com>
2019-04-24 19:28:56 +0100

    mocha tests

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const connectMongoDB = require("./src/model/db/mongoConfig.js").connectMongoDB;
 const mongoDBConfig = require("./src/model/db/mongoConfig.js").mongoDBConfig;
 const passport = require("passport");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require('cookie-parser');
+const path = require("path");
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
+
 connectMongoDB(() => {
     const User = mongoDBConfig.collections[0].model;
     require('./src/model/passport')(passport);
     passport.use(new LocalStrategy(User.authenticate()));
-    
+
     /*
         passport.use(new LocalStrategy(
             function (email, password, done) {
@@ -21,53 +29,47 @@ connectMongoDB(() => {
                 });
             }
         )); */
-/*
-    passport.use(
-        'local-signup',
-        new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField: 'username',
-            passwordField: 'password',
-            passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-            function (req, username, password, done) {
-                // find a user whose email is the same as the forms email
-                // we are checking to see if the user trying to login already exists
-                connection.query("SELECT * FROM users WHERE username = ?", [username], function (err, rows) {
-                    if (err)
-                        return done(err);
-                    if (rows.length) {
-                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-                    } else {
-                        // if there is no user with that username
-                        // create the user
-                        var newUserMysql = {
-                            username: username,
-                            password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
-                        };
+    /*
+        passport.use(
+            'local-signup',
+            new LocalStrategy({
+                // by default, local strategy uses username and password, we will override with email
+                usernameField: 'username',
+                passwordField: 'password',
+                passReqToCallback: true // allows us to pass back the entire request to the callback
+            },
+                function (req, username, password, done) {
+                    // find a user whose email is the same as the forms email
+                    // we are checking to see if the user trying to login already exists
+                    connection.query("SELECT * FROM users WHERE username = ?", [username], function (err, rows) {
+                        if (err)
+                            return done(err);
+                        if (rows.length) {
+                            return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                        } else {
+                            // if there is no user with that username
+                            // create the user
+                            var newUserMysql = {
+                                username: username,
+                                password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
+                            };
+    
+                            var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
+    
+                            connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
+                                newUserMysql.id = rows.insertId;
+    
+                                return done(null, newUserMysql);
+                            });
+                        }
+                    });
+                })
+        );*/
 
-                        var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
-
-                        connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err, rows) {
-                            newUserMysql.id = rows.insertId;
-
-                            return done(null, newUserMysql);
-                        });
-                    }
-                });
-            })
-    );*/
     passport.serializeUser(User.serializeUser());
     passport.deserializeUser(User.deserializeUser());
 });
-const express = require("express");
-const bodyParser = require("body-parser");
-const routes = require("./src/routes/api.js");
-const session = require("express-session");
-var cookieParser = require('cookie-parser');
-const path = require("path");
-const LocalStrategy = require('passport-local').Strategy;
-const flash = require('connect-flash');
+
 
 
 let app = express();
@@ -99,14 +101,13 @@ require('./src/routes/api.js')(app, passport); // load our routes and pass in ou
 //app.use('/api', routes);
 
 
-
-
-let server = app.listen(8080, function () {
+const server = app.listen(8080, function () {
     const host = server.address().address === "::"
         ? "localhost"
         : server.address().address;
     const port = server.address().port;
 
     console.log("App listening at http://%s:%s", host, port);
-})
+});
 
+module.exports.app = app;
---
+++ test/00_set_server.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log("running set_server.js");
+
+require("../app.js");
+
