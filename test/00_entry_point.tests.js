ca8671c6725ac70fd99ef5c4169940a05372c134
navi <vosqueentrais@gmail.com>
2019-05-17 12:43:22 +0100

    entry point tests

--- test/00_entry_point.tests.js
+++ test/00_entry_point.tests.js
@@ -5,15 +5,22 @@ console.log("running 00_entry.point.js");
 require("../app.js");
 
 describe("", function () {
-    this.timeout(10000);
+    this.timeout(15000);
     const mongoDBConfig = require("../src/model/db/mongoConfig").mongoDBConfig;
+    //const mongoose = require("mongoose");
     before(function (done) {
         setTimeout(function () {
             let httpTests = require("./tests/http.tests");
             let mongoUserTests = require("./tests/mongo.users.tests")(mongoDBConfig.collections[0]);
             let mongoAnimalsTests = require("./tests/mongo.animals.tests")(mongoDBConfig.collections[1]);
+            //mongoDBConfig.mongoose.disconnect();
             done();
-        }, 8000);
+        }, 6000);
+        //setTimeout(function () {
+            //mongoDBConfig.connection.close();
+            //mongoDBConfig.mongoose.disconnect();
+            //done();
+        //}, 20000);
     });
     it('', function (done) {
         done();
